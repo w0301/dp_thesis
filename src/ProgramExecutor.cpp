f26f2fd11d02e7489d545a4d0928366f0ca309b7
Richard Kakaš <richard.kakas@gmail.com>
2018-04-11 23:50:49 +0200

    Add some classes interfaces for program executor.

--- src/ProgramExecutor.cpp
+++ src/ProgramExecutor.cpp
@@ -1,1 +1,23 @@
 #include "ProgramExecutor.h"
+
+using namespace std;
+
+// Objects
+shared_ptr<ExecValue> ExecObject::getFieldByPath(const string& path) const {
+    // TODO
+    return std::shared_ptr<ExecValue>();
+}
+
+void ExecObject::setFieldByPath(const string& path, shared_ptr<ExecValue> val) {
+    // TODO
+}
+
+// Executor
+shared_ptr<ExecValue> ProgramExecutor::exec(shared_ptr<ExecObject> args) {
+    return execFunction(program->getFunction("main"), std::move(args));
+}
+
+shared_ptr<ExecValue> ProgramExecutor::execFunction(shared_ptr<Function> function, shared_ptr<ExecObject> local) {
+    // TODO
+    return shared_ptr<ExecValue>();
+}
--- src/ProgramExecutor.h
+++ src/ProgramExecutor.h
@@ -1,8 +1,79 @@
 #ifndef PROGRAMEXECUTOR_H
 #define PROGRAMEXECUTOR_H
 
+#include <map>
+#include <string>
+#include <memory>
 
+#include "Program.h"
+
+// Objects
+class ExecValue {
+
+};
+
+class ExecObject : public ExecValue {
+public:
+    std::shared_ptr<ExecValue> getFieldByPath(const std::string&) const;
+    void setFieldByPath(const std::string&, std::shared_ptr<ExecValue>);
+
+    std::shared_ptr<ExecValue> getField(const std::string& name) {
+        return fields[name];
+    }
+
+    void setField(const std::string& name, std::shared_ptr<ExecValue> val) {
+        fields[name] = val;
+    }
+
+private:
+    std::map<std::string, std::shared_ptr<ExecValue> > fields;
+};
+
+class ExecPrimitive : public ExecValue {
+};
+
+template<class T> class ExecPrimitiveTemplate : public ExecPrimitive {
+public:
+    const T& getValue() const {
+        return value;
+    }
+
+    void setValue(const T& value) {
+        ExecPrimitiveTemplate::value = value;
+    }
+
+private:
+    T value;
+};
+
+class ExecBoolean : public ExecPrimitiveTemplate<bool> {
+};
+
+class ExecInteger : public ExecPrimitiveTemplate<long long> {
+};
+
+class ExecFloat : public ExecPrimitiveTemplate<float> {
+};
+
+class ExecChar : public ExecPrimitiveTemplate<char32_t > {
+};
+
+class ExecString : public ExecPrimitiveTemplate<std::u32string> {
+};
+
+// Executor
 class ProgramExecutor {
+public:
+    ProgramExecutor(std::shared_ptr<Program> program) :
+            program(std::move(program)), global(std::make_shared<ExecObject>()) { }
+
+    std::shared_ptr<ExecValue> exec(std::shared_ptr<ExecObject>);
+
+private:
+    std::shared_ptr<ExecValue> execFunction(std::shared_ptr<Function>, std::shared_ptr<ExecObject>);
+
+    std::shared_ptr<Program> program;
+    std::shared_ptr<ExecObject> global;
 
 };
 
